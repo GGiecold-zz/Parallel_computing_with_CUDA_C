2978fd105d14e4241b853879c110304c3afcb587
Gregory Giecold <GGiecold@users.noreply.github.com>
2017-07-25 22:32:23 -0400

    Create my_gl.h

---
+++ Common/my_gl.h
@@ -0,0 +1,13 @@
+#ifndef __MY_GL_H__
+#define __MY_GL_H__
+
+
+#include <GL/glut.h>
+#include <GL/glext.h>
+#include <GL/glx.h>
+
+
+#define GET_PROC_ADDRESS(s) glXGetProcAddress((const GLubyte *) s)
+
+
+#endif
